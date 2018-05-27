583a28ff9aaf536588c446e880f43f02da491281
Andy Li <lidi_1985@126.com>
2018-05-27 20:47:18 +0800

    第二次提交代码

--- src/main/java/com/example/springbootproject/SpringbootprojectApplication.java
+++ src/main/java/com/example/springbootproject/SpringbootprojectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootprojectApplication {
 
     public static void main(String[] args) {
-
+        //this will start the project
         SpringApplication.run(SpringbootprojectApplication.class, args);
+
     }
 }
